cc1f31697e21a02bb9790056913b6989de76306f
muhammad usama <mohammadusama985@gmail.com>
2026-03-17 13:45:37 +0500

    updated

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://freely-childhood-tile-riverside.trycloudflare.com/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://holiday-occurrence-grace-display.trycloudflare.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
